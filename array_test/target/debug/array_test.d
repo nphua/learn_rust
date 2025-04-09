1ea4fe67d2816907adb1882ea54feb101e208d26
Nicholas Phua <nphua@pdx.edu>
2025-04-09 16:25:03 -0700

    messing around with arrays

---
+++ array_test/target/debug/array_test.d
@@ -0,0 +1,1 @@
+/home/john/Documents/emb_rust/learn_rust/array_test/target/debug/array_test: /home/john/Documents/emb_rust/learn_rust/array_test/src/main.rs
---
+++ array_test/target/debug/deps/array_test-84f3ff701c6438b9.d
@@ -0,0 +1,5 @@
+/home/john/Documents/emb_rust/learn_rust/array_test/target/debug/deps/array_test-84f3ff701c6438b9: src/main.rs
+
+/home/john/Documents/emb_rust/learn_rust/array_test/target/debug/deps/array_test-84f3ff701c6438b9.d: src/main.rs
+
+src/main.rs:
